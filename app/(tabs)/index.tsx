8393c84d9cadf394bb22418172776f8b143d737e
animeshhh17 <animeshkumarraikwar@gmail.com>
2024-09-10 19:32:29 +0530

    FlatList

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,20 @@
 import { Stack } from 'expo-router';
 import EventListitem from '~/components/EventListitem';
-
+import { FlatList } from 'react-native';
 import events from '~/assets/events.json';
 
 export default function Events() {
   return (
     <>
       <Stack.Screen options={{ title: 'Events' }} />
-      <EventListitem event={events[0]} />
+
+      <FlatList data={events} renderItem={({ item }) => <EventListitem event={item} />} />
+
+      {/* <EventListitem event={events[0]} />
       <EventListitem event={events[1]} />
       <EventListitem event={events[2]} />
       <EventListitem event={events[3]} />
-      <EventListitem event={events[4]} />
+      <EventListitem event={events[4]} /> */}
     </>
   );
 }
